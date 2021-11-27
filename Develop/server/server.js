4ac5ddcd8e2c3c6c3a5589a9140413e85ef6c082
Sun <solskinartwork@gmail.com>
2021-11-27 14:27:01 -0500

    moving files

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-const db = require('../config/connection');
-const { authMiddleware } = require('./auth');
+const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const { ApolloServer } = require('apollo-server-express');
 const app = express();
