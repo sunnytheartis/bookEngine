e595d160a9798cfa1774e005e86b28bbe516c979
Sun <solskinartwork@gmail.com>
2021-11-29 17:11:00 -0500

    editing app.js

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+
+import { setContext } from '@apollo/client/link/context'
+
+import SearchBooks from './pages/SearchBooks'
+import SavedBooks from './pages/SavedBooks'
+import Navbar from './components/Navbar'
+
+var link = createHttpLink({
+  uri: '/graphql',
+}); 
+let authLink = setContext((_, { headers }) => {
+  
+  let token = localStorage.getItem('id_token');
+  
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+let apolloClient = new ApolloClient({
+  link: authLink.concat(link),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
+    <ApolloProvider client={apolloClient}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +41,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
